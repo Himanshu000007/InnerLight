c5ba8b05a4b7eef925db29a92f03fd673e894401
Himanshu <himanshu8809564335@gmail.com>
2026-06-17 16:43:10 +0530

    fix : used production api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/v1';
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://innerlight-srip.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
